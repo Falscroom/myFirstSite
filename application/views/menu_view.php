6b20e95d181c33c6a4176458dcd6f55265be179f
falscroom <falscroom@gmail.com>
2014-12-25 20:14:24 +1000

    Работа над меню продолжается.

--- application/views/menu_view.php
+++ application/views/menu_view.php
@@ -19,7 +19,15 @@
                     <ul>
                         <li></li>
                     <? foreach($data['menu_items'][2] as $arr_level_two): ?>
-                        <li><?if($arr_level_two['lft'] > $arr['lft'] && $arr_level_two['rght'] < $arr['rght']) echo $arr_level_two['name']?></li>
+                            <? if($arr_level_two['lft'] > $arr['lft'] && $arr_level_two['rght'] < $arr['rght']): ?>
+                            <li><?=$arr_level_two['name']?></li>
+                            <div class="ac_level_three">
+                                <ul>
+                                    <li> 11 </li>
+                                    <li> 22 </li>
+                                </ul>
+                            </div>
+                            <? endif ?>
                     <? endforeach; ?>
                     </ul>
                 </div>
